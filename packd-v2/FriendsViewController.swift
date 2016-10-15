cb1d9a48ffa6d33f802953f67a171e640b6fda28
cam39porter <cam.b.porter@gmail.com>
2016-10-15 16:50:13 -0500

    added friends view controller datasource code

--- packd-v2/FriendsViewController.swift
+++ packd-v2/FriendsViewController.swift
@@ -9,5 +9,45 @@
 import UIKit
 
 class FriendsViewController: PageableViewController {
-
+    // START: Model
+    var friends = [User]()
+    let cellReuseIdentifier = "friendCell"
+    // END: Model
+    
+    // START: View
+    override func viewDidLoad() {
+        collectionView?.register(FriendCell.self, forCellWithReuseIdentifier: cellReuseIdentifier)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        fetchData()
+    }
+    // END: View
+    
+    // START: Collection View Datasource
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellReuseIdentifier, for: indexPath) as! FriendCell
+        
+        // set up the cell
+        
+        return cell
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return friends.count
+    }
+    
+    private func fetchFriends() {
+        User.getAllUsers { (friend) in
+            self.friends.append(friend!)
+            self.collectionView?.reloadData()
+        }
+    }
+    
+    override func fetchData() {
+        fetchFriends()
+    }
+    // END: Collection View Datasource
+    
+    
 }
