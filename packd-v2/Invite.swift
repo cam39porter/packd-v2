728d52d7ec5b6728c813aaae16f4ed6927ba348f
cam39porter <cam.b.porter@gmail.com>
2016-11-08 11:05:34 -0800

    creating invite model

---
+++ packd-v2/Invite.swift
@@ -0,0 +1,44 @@
+//
+//  Invite.swift
+//  packd-v2
+//
+//  Created by Cameron Porter on 11/8/16.
+//  Copyright © 2016 Cameron Porter. All rights reserved.
+//
+
+import UIKit
+import FirebaseDatabase
+
+class Invite: DatabaseObject {
+    
+    // Start: Attributes
+    var uid: String?
+    var senderUID: String?
+    var recipientUIDs = [String?]()
+    var establishmentUIDs = [String?]()
+    var startTime: NSNumber?
+    // END: Attributes
+    
+    // START: database -> invites
+    static let invitesReference = DatabaseObject.ref?.child("invites")
+    
+    static let inviteToSnapshotCompletion:(@escaping (Invite?) -> Void) -> ((FIRDataSnapshot?) -> Void) = { (inviteCompletion) in
+        let completion: (FIRDataSnapshot?) -> Void = { (snapshot) in
+            if let inviteInfoDictionary = snapshot?.value as? [String:AnyObject] {
+                let invite = Invite()
+                invite.uid = snapshot?.key
+                invite.senderUID = inviteInfoDictionary["senderUID"] as? String
+                
+                inviteCompletion(invite)
+            }
+        }
+        return completion
+    }
+    
+    static func getAllInvites(withCompletionHandler completion: @escaping (Invite?) -> Void) {
+        DatabaseObject.objectsReference = invitesReference
+        let inviteCompletion  = inviteToSnapshotCompletion(completion)
+        DatabaseObject.getAllObjects(withCompletionHandler: inviteCompletion)
+    }
+    // END: database -> invites
+}
